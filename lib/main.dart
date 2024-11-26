60d2c1ec9936ad160dfaf57c40204a019610d610
taha <tnttasata@gmail.com>
2024-11-26 03:20:06 +0300

    add home view

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/Mobile/views/home_view.dart';
 
 class QuizApp extends StatelessWidget {
   const QuizApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
   }
 }
