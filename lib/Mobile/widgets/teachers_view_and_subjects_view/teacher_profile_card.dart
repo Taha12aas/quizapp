e7786ace79d48881495de5814d747466a997a070
taha <asdfghmnt@gmail.com>
2024-11-30 22:43:26 +0300

    edit TeacherProfileCard

--- lib/Mobile/widgets/teachers_view_and_subjects_view/teacher_profile_card.dart
+++ lib/Mobile/widgets/teachers_view_and_subjects_view/teacher_profile_card.dart
@@ -13,45 +13,49 @@ class TeacherProfileCard extends StatelessWidget {
     // final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
 
-    return Container(
-      width: screenWidth * 0.6,
-      padding: EdgeInsets.symmetric(vertical: screenWidth * 0.001),
-      decoration: BoxDecoration(
-        color: kAshenColor,
-        borderRadius: BorderRadius.circular(60),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'ريم سواس',
-                  style: FontStyleApp.textStylewite15.copyWith(
-                    fontSize: getResponsiveText(context, 15),
+    return InkWell(
+      borderRadius: BorderRadius.circular(50),
+      onTap: () {},
+      child: Container(
+        width: screenWidth * 0.6,
+        padding: EdgeInsets.symmetric(vertical: screenWidth * 0.001),
+        decoration: BoxDecoration(
+          color: kAshenColor,
+          borderRadius: BorderRadius.circular(60),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'ريم سواس',
+                    style: FontStyleApp.textStylewite15.copyWith(
+                      fontSize: getResponsiveText(context, 15),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  ' : الاسم',
-                  style: FontStyleApp.textStyleOrange15.copyWith(
-                    fontSize: getResponsiveText(context, 15),
+                  const SizedBox(
+                    width: 5,
                   ),
-                ),
-              ],
+                  Text(
+                    ' : الاسم',
+                    style: FontStyleApp.textStyleOrange15.copyWith(
+                      fontSize: getResponsiveText(context, 15),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          const CircleAvatar(
-            radius: 30.0,
-            backgroundImage: AssetImage('assets/images/TeachersTaha.jpg'),
-          ),
-        ],
+            const SizedBox(
+              width: 10,
+            ),
+            const CircleAvatar(
+              radius: 30.0,
+              backgroundImage: AssetImage('assets/images/TeachersTaha.jpg'),
+            ),
+          ],
+        ),
       ),
     );
   }
